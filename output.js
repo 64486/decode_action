654b1cdcc554f0bec6acd36bdddfe6a628c1344a
action <action@github.com>
2024-10-17 11:43:47 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Mon Oct 14 2024 16:05:36 GMT+0000 (Coordinated Universal Time)
+//Thu Oct 17 2024 11:43:46 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 NAME = "巨推百惠";
